7443c42a89e5eec62df525a47c92577a401669b0
Vadim Arsenev <vvarsenev@gmail.com>
2023-02-23 17:13:14 +0300

    added national teams for the World Cup

---
+++ settings/worldcup.py
@@ -0,0 +1,23 @@
+
+__author__ = 'Vadim Arsenev'
+__version__ = '0.9.0'
+__data__ = '19.11.2022'
+
+
+# additional url for each market - constant
+# format: markets_url = 'https://www.marathonbet.com/en/markets.htm'
+MARKETS_URL = 'https://www.marathonbet.com/en/markets.htm'
+
+# World Cup 2022
+TOURNAMENTS_URL = 'https://www.marathonbet.com/en/popular/Football/Internationals/FIFA+World+Cup/2022/Final+Tournament/Qatar/Group+Stage+-+13372800?lid=14731352'
+
+# for odds only
+COLUMNS = './settings/odds.txt'
+# for terminal and .exe in Windows
+RESULT_FILE = ['./data/oddsWC.csv']
+
+# team names
+TEAMS = './settings/teamnames.txt'
+# abbr or team
+TEAM_NAMES = 'abbr'
+# TEAM_NAMES = 'team'
