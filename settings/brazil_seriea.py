ee848f2aec6302e4c0348bd5592a341133ac6f4f
Vadim Arsenev <vvarsenev@gmail.com>
2023-05-29 18:03:20 +0300

    added Brazil Serie A

---
+++ settings/brazil_seriea.py
@@ -0,0 +1,23 @@
+
+__author__ = 'Vadim Arsenev'
+__version__ = '0.9.0'
+__data__ = '29.05.2023'
+
+
+# additional url for each market - constant
+# format: markets_url = 'https://www.marathonbet.com/en/markets.htm'
+MARKETS_URL = 'https://www.marathonbet.com/en/markets.htm'
+
+# England. Premier League
+TOURNAMENTS_URL = 'https://www.marathonbet.com/en/popular/Football/Brazil/Serie+A+-+211229?interval=ALL_TIME'
+
+# for odds only
+COLUMNS = './settings/odds.txt'
+# for terminal and .exe in Windows
+RESULT_FILE = ['./data/oddsBrazil.csv']
+
+# team names
+TEAMS = './settings/teamnames.txt'
+# abbr or team
+TEAM_NAMES = 'abbr'
+# TEAM_NAMES = 'team'
